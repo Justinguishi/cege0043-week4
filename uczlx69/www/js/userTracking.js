987581f4c10928d96882c109856d8d57f8723da6
Kris Wong <shellenwong@hotmail.com>
2019-02-07 09:15:51 -0500

    lol

--- uczlx69/www/js/userTracking.js
+++ uczlx69/www/js/userTracking.js
@@ -1,11 +1,12 @@
-function trackAndCircle(){
+var userMarker
+
+function trackLocation(){
 if (navigator.geolocation){
 navigator.geolocation.watchPosition(showPosition);}
 else{
 document.getElementById('showLocation').innerHTML='Geolocation is not supported by this browser'}}
 
 function showPosition(position){
-document.getElementById('showLocation').innerHTML='Latitude: '+position.coords.latitude+
-'<br>Longitude: '+position.coords.longitude;}
+	if (userMarker){mymap.removeLayer(userMarker);}
+userMarker=L.marker([position.coords.latitude,position.coords.longitude]).addTo(mymap).bindPopup('<b>I am here!</b>');}
 
-L.marker([position.coords.latitude,position.coords.longitude]).addTo(mymap).bindPopup('<b>I am here!</b>');
